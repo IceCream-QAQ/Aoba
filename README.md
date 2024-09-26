046a639c5f8df6934d0ebea3b3ee7621a50d6a43
icecream <www@withdata.net>
2024-09-26 18:35:49 +0800

    移除所依赖的 jaxb

--- README.md
+++ README.md
@@ -15,9 +15,4 @@ Aoba 将会提供对应的 Async 与 Blocking API，以方便 Java 开发者使�
 Aoba 通过 `kotlinx-serialization-json` 进行 JSON 序列化与反序列化。  
 Spring 会判断是否存在 `kotlinx-serialization-json`。  
 如果存在则会 优先 对 `kotlinx-serialization-json` 支持的类 使用 `kotlinx-serialization-json` 进行 JSON 序列化与反序列化。  
-对于存在 后处理器 修改返回值的情况，请手动配置 Spring MVC 序列化器，否则可能产生叠加效果导致异常。  
-
-## 对于 Java 9+ 用户
-
-部分情况（将 ByteArray 转为 HexString）的过程依赖 `jaxb-api`，Java 9+ 不再默认包含 `jaxb-api`，请手动添加依赖。  
-后续可能会移除对 `jaxb-api` 的依赖。
+对于存在 后处理器 修改返回值的情况，请手动配置 Spring MVC 序列化器，否则可能产生叠加效果导致异常。
--- core/src/main/kotlin/aoba/fun/hex.kt
+++ core/src/main/kotlin/aoba/fun/hex.kt
@@ -4,7 +4,6 @@ import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
-import javax.xml.bind.DatatypeConverter
 
 fun hmac256(key: ByteArray, msg: String): ByteArray {
     val mac = Mac.getInstance("HmacSHA256")
@@ -16,8 +15,26 @@ fun hmac256(key: ByteArray, msg: String): ByteArray {
 fun sha256Hex(s: String): String {
     val md = MessageDigest.getInstance("SHA-256")
     val d = md.digest(s.toByteArray(StandardCharsets.UTF_8))
-    return DatatypeConverter.printHexBinary(d).toLowerCase()
+    return hexStringL(d)
 }
 
 fun sign(secretSigning: ByteArray, stringToSign: String) =
-    DatatypeConverter.printHexBinary(hmac256(secretSigning, stringToSign)).toLowerCase()
+    hexStringL(hmac256(secretSigning, stringToSign))
+
+private val hexCodeU = "0123456789ABCDEF".toCharArray()
+private val hexCodeL = "0123456789abcdef".toCharArray()
+
+fun hexStringU(data: ByteArray): String =
+    hexString(data, hexCodeU)
+
+fun hexStringL(data: ByteArray): String =
+    hexString(data, hexCodeL)
+
+private fun hexString(data: ByteArray, hexCode: CharArray): String {
+    val r = StringBuilder(data.size * 2)
+    for (b in data) {
+        r.append(hexCode[(b.toInt() shr 4) and 0xF])
+        r.append(hexCode[(b.toInt() and 0xF)])
+    }
+    return r.toString()
+}
