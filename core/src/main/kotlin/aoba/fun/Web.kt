51fff854d2521629bd9149ec7e1a0231f3e9f3aa
icecream <www@withdata.net>
2025-08-07 07:52:59 +0800

    修改 Web 中提供的默认函数

--- core/src/main/kotlin/aoba/fun/Web.kt
+++ core/src/main/kotlin/aoba/fun/Web.kt
@@ -1,51 +1,63 @@
 package aoba.`fun`
 
 import kotlinx.coroutines.CompletableDeferred
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import okio.BufferedSink
 import java.io.IOException
 
 @JvmInline
 value class Web(
     private val client: OkHttpClient = OkHttpClient()
 ) {
+    object EmptyBody : RequestBody() {
+        override fun contentType(): MediaType? = null
+        override fun contentLength(): Long = 0
+        override fun writeTo(sink: BufferedSink) {}
+    }
 
-    class RequestBuilder(url: String) {
+    @JvmInline
+    value class RequestBuilder(private val rb: Request.Builder = Request.Builder()) {
         @JvmInline
         value class HeaderBuilder(private val builder: Request.Builder) {
             infix fun String.to(that: String) = builder.addHeader(this, that)
         }
 
-        private val rb: Request.Builder = Request.Builder()
-
-        init {
-            if (url != "") rb.url(url)
-        }
+        constructor(url: String) : this(Request.Builder().url(url))
 
         fun headers(body: HeaderBuilder.() -> Unit) {
             HeaderBuilder(rb).apply(body)
         }
 
         fun url(url: String) = rb.url(url)
-        fun json(body: String) = rb.post(body.toRequestBody("application/json".toMediaType()))
+
+        fun json(body: String, method: String = "POST") =
+            rb.method(method, body.toRequestBody("application/json".toMediaType()))
+
+        fun method(method: String, body: RequestBody = EmptyBody) =
+            rb.method(method, body)
+
         fun form() = Unit
         fun formData() = Unit
 
         fun build() = rb.build()
     }
 
-
     suspend fun execute(request: Request): Response {
         val wait = CompletableDeferred<Response>()
-        client.newCall(request).enqueue(object : okhttp3.Callback {
-            override fun onFailure(call: okhttp3.Call, e: IOException) {
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
                 wait.completeExceptionally(e)
             }
 
-            override fun onResponse(call: okhttp3.Call, response: Response) {
+            override fun onResponse(call: Call, response: Response) {
                 wait.complete(response)
             }
         })
