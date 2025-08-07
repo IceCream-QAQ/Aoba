c9fea6df6da11adc5b8cb2d60c6720a5e96fabf3
icecream <www@withdata.net>
2025-08-07 08:40:48 +0800

    实现 删除&查询 记录

--- isp/tencent/src/main/kotlin/aoba/isp/tencent/dnspod/Record.kt
+++ isp/tencent/src/main/kotlin/aoba/isp/tencent/dnspod/Record.kt
@@ -10,9 +10,9 @@ class Record(private val qCloud: TencentCloud) {
 
     @Serializable
     data class AddRecordResp(
-        @SerialName("RequestId")
-        val recordId: String,
         @SerialName("RecordId")
+        val recordId: String,
+        @SerialName("RequestId")
         val requestId: String
     )
 
@@ -41,6 +41,106 @@ class Record(private val qCloud: TencentCloud) {
             "Weight" to weight
             "MX" to mx
             "Remark" to remark
+        }
+    )
+
+    @Serializable
+    data class DeleteRecordResp(
+        @SerialName("RequestId")
+        val requestId: String
+    )
+
+    suspend fun deleteRecord(
+        domain: String,
+        recordId: String
+    ) = qCloud.post<DeleteRecordResp>(
+        "dnspod",
+        "DeleteRecord",
+        "",
+        "2021-03-23",
+        buildJson {
+            "Domain" to domain
+            "RecordId" to recordId.toLong()
+        }
+    )
+
+    @Serializable
+    data class ListResp(
+        @SerialName("RecordCountInfo")
+        val page: Page,
+        @SerialName("RecordList")
+        val list: List<RecordItem>,
+        @SerialName("RequestId")
+        val requestId: String
+    ) {
+        @Serializable
+        data class Page(
+            @SerialName("SubDomainCount")
+            val subDomainCount: Int? = null,
+            @SerialName("ListCount")
+            val listCount: Int,
+            @SerialName("TotalCount")
+            val totalCount: Int,
+        )
+
+        @Serializable
+        data class RecordItem(
+            @SerialName("RecordId")
+            val recordId: String,
+            @SerialName("Value")
+            val value: String,
+            @SerialName("Status")
+            val status: String,
+            @SerialName("UpdatedOn")
+            val updateOn: String? = null,
+            @SerialName("Name")
+            val name: String,
+            @SerialName("Line")
+            val line: String,
+            @SerialName("LineId")
+            val lineId: String? = null,
+            @SerialName("Type")
+            val type: String,
+            @SerialName("Weight")
+            val weight: Int? = null,
+            @SerialName("MonitorStatus")
+            val monitorStatus: String? = null,
+            @SerialName("Remark")
+            val remark: String? = null,
+            @SerialName("TTL")
+            val ttl: Int,
+            @SerialName("MX")
+            val mx: Int? = null
+        )
+    }
+
+    suspend fun list(
+        domain: String,
+        sub: String? = null,
+        type: RecordType? = null,
+        line: String? = null,
+        groupId: Int? = null,
+        keyword: String? = null,
+        sortField: String? = null,
+        sortType: String? = null,
+        offset: Int? = null,
+        limit: Int? = null
+    ) = qCloud.post<ListResp>(
+        "dnspod",
+        "DescribeRecordList",
+        "",
+        "2021-03-23",
+        buildJson {
+            "Domain" to domain
+            "Subdomain" to sub
+            "RecordType" to type?.value
+            "RecordLine" to line
+            "GroupId" to groupId
+            "Keyword" to keyword
+            "SortField" to sortField
+            "SortType" to sortType
+            "Offset" to offset
+            "Limit" to limit
         }
     )
 
